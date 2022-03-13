5e445cef89cdecfcdff78de638adae506de0773d
LENOVO <LENOVO@LAPTOP-364SR7O4>
2022-03-13 12:07:23 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Methods 
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+     
+            Product product1= new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya Elması";
+
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("---------------------------");
+            }
+
+            Console.WriteLine("-----------Methods-----------");    
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(product1);
+            sepetManager.Add(product2);
+
+
+            sepetManager.Add2("Armut", "Yeşil Armut", 12, 9);
+            sepetManager.Add2("Elma", "Yeşil Elma", 12, 8);
+            sepetManager.Add2("Karpuz", "Diyarbakır Karpuzu", 12, 3);
+
+
+
+
+
+            // Console.WriteLine("Hello, World!");
+
+        }
+        
+    }
+
+}
+
+
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class SepetManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi :" + product.Adi, product.Fiyati, product.Aciklama, product.stokAdedi);
+        }
+
+
+        public void Add2(string productAdi, string productaciklama, double productfiyat, int productstokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi :" + productAdi);
+        }
+
+    }
+}
